c433ebed4bd68a94898898d69a315a416bd9d331
Karol Selak <karol.selak@bowwe.com>
2025-03-02 16:41:05 +0100

    fix: flatted instead of JSON.stringify

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const { stringify } = require('flatted');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
 app.get('/getTest/:param/test', (req, res) => {
-    res.send('Hello getTest<br><br>' + JSON.stringify(req));
+    res.send('Hello getTest<br><br>' + stringify(req));
 });
 
 app.post('/postTest/:param/test', (req, res) => {
-    res.send('Hello postTest<br><br>' + JSON.stringify(req));
+    res.send('Hello postTest<br><br>' + stringify(req));
 });
 
 app.listen(PORT, () => {
