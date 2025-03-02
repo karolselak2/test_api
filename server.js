b2a770eac777aa25d6b6ec7410c94419d19c8c87
Karol Selak <karol.selak@bowwe.com>
2025-03-02 16:32:46 +0100

    feat: gestTest and postTest

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.get('/test', (req, res) => {
-    res.send('Hello test');
+app.get('/getTest/:param/test', (req, res) => {
+    res.send('Hello getTest\n\n' + JSON.stringify(req.params));
+});
+
+app.post('/postTest/:param/test', (req, res) => {
+    res.send('Hello postTest\n\n' + JSON.stringify(req.params));
 });
 
 app.listen(PORT, () => {
